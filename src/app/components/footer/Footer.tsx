a5386004d969ccad896d397aa2c2676f6f0275cd
Morten "BomBom" Hansen <morten@bomholdt.net>
2024-12-04 13:39:37 +0100

    Tilføjet stile fra figma

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <>
-      <Image src="../../" alt="Din Maegler" height={500} width={500} />
+      <Image src="/dinMaegler.svg" alt="Din Maegler" height={500} width={500} />
+      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
     </>
   );
 }
