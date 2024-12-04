49d9f6af0291f357334cbfe874c91a1c51ec0918
Morten "BomBom" Hansen <morten@bomholdt.net>
2024-12-04 14:01:43 +0100

    Opdateret footer komponent med ny tekststil

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <Image src="/dinMaegler.svg" alt="Din Maegler" height={500} width={500} />
-      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
+      <p className="text-style-paragraph-para-01">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.</p>
     </>
   );
 }
