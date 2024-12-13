4709c7e8cff545398dabefbf236c128664af927d
Morten 'BomBom' Hansen <morten@bomholdt.net>
2024-12-13 12:10:19 +0100

    1up

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
-import Footer from './components/footer/Footer';
 import { Header } from './components/header/header';
+import Footer from './components/footer/Footer';
 import ErrorContent from './components/error-content';
 
 export default function notFound() {
