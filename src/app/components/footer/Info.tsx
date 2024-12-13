b7086b4d715e30f0c0926ff03004669c8614e3a6
Morten 'BomBom' Hansen <morten@bomholdt.net>
2024-12-13 15:10:59 +0100

    Fix typo

--- src/app/components/footer/Info.tsx
+++ src/app/components/footer/Info.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Info: React.FC<InfoProps> = ({ icon, element, mainText, subText }) => {
   return (
-    <div className="flex items-center space-x-4s">
+    <div className="flex items-center space-x-4">
       {icon ? icon : element}
       <div>
         <p className="font-bold text-style-paragraph-para-01 text-paragraph-para-01">{mainText}</p>
